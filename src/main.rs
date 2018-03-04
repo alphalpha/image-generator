c562a96281625a555660652db4be524fbf3ed475
alphalpha <christian@sineswingers.com>
2018-03-04 23:47:44 +0100

    Add support for analyzing specific area

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,20 @@ extern crate rusttype;
 use std::path::{Path, PathBuf};
 use std::env;
 use std::{fs, io};
-use imageproc::drawing::draw_text_mut;
+use imageproc::drawing::{draw_text_mut, draw_hollow_rect_mut};
+use imageproc::rect::Rect;
 use image::{DynamicImage, GenericImage, Rgb, RgbImage};
 use rusttype::{FontCollection, Scale};
 
 
-fn mean_color(img : &DynamicImage) -> Result<Rgb<u8>, io::Error> {
-    let dimensions = img.dimensions();
-    let num_pixels = dimensions.0 * dimensions.1;
+fn mean_color(img : &DynamicImage, rect : &Rect) -> Result<Rgb<u8>, io::Error> {
+    let num_pixels = rect.width() * rect.height();
 
     let color : Vec<u8> = img.pixels()
+        .filter(|f|(f.0 >= rect.left() as u32
+            && f.0 < (rect.left() as u32 + rect.width() as u32))
+            && (f.1 >= rect.top() as u32
+            && f.1 < (rect.top() as u32 + rect.height() as u32)))
         .fold(vec![0u32, 0u32, 0u32], |mut acc, pixel| {
             for i in 0..acc.len() { acc[i] += pixel.2[i] as u32; }
             acc
@@ -44,7 +48,12 @@ fn main() {
     if !metadata.is_dir() {
         panic!("Must be a directory");
     }
+    let default_x = 100;
+    let default_y = 100;
+    let default_width = 100;
+    let default_height = 100;
 
+    let rect = Rect::at(default_x, default_y).of_size(default_width, default_height);
     let font = Vec::from(include_bytes!("DejaVuSans.ttf") as &[u8]);
     let font = FontCollection::from_bytes(font).into_font().expect("Could not load font");
     let height = 22.4;
@@ -61,12 +70,14 @@ fn main() {
             + in_path.extension().unwrap().to_str().unwrap();
         let in_image = image::open(&in_path).expect("Opening image failed");
 
-        let color = mean_color(&in_image).expect("Could not calculate mean color");
+        let color = mean_color(&in_image, &rect).expect("Could not calculate mean color");
         let mut image = RgbImage::new(in_image.dimensions().0, in_image.dimensions().1);
         for p in image.pixels_mut() { *p = color; }
 
         let text = in_path.file_stem().unwrap().to_str().unwrap();
         draw_text_mut(&mut image, Rgb([255u8, 255u8, 255u8]), 10, 10, scale, &font, text);
+
+        //draw_hollow_rect_mut(&mut image, rect, Rgb([255u8, 255u8, 255u8]));
 
         let _ = image.save(file_name).expect("Could not save file");
     }
