f6c61882d82829cb76420303de32c0d309db73e2
alphalpha <christian@sineswingers.com>
2018-03-12 22:35:45 +0100

    Write error to standard error

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ use image_generator::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|e| {
-        println!("Problem parsing arguments: {}", e);
+        eprintln!("Problem parsing arguments: {}", e);
         process::exit(1);
     });
 
     if let Err(e) = image_generator::run(config) {
-        println!("Application Error: {}", e);
+        eprintln!("Application Error: {}", e);
         process::exit(1);
     }
 }
