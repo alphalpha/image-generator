9ae67ba1634458025bbc401f59fc6abaf7a4c6a8
alphalpha <christian@sineswingers.com>
2018-03-20 16:58:58 +0100

    Show help on parsing error

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ use std::env;
 use std::process;
 use image_generator::Config;
 
+fn help() -> String {
+    String::from("usage: \
+    image-generator [<images_path>] [<font_path] [<x-coordinate> <y-coordinate> <width> <height>]\n\
+    example: image-generator src/test_data src/DejaVuSans.ttf 100 100 100 100")
+}
+
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|e| {
-        eprintln!("Problem parsing arguments: {}", e);
+        eprintln!("Problem parsing arguments. {}\n\n{}", e, help());
         process::exit(1);
     });
 
