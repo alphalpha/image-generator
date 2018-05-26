bc263a6de073d710303947d5b891656d04dfa090
alphalpha <christian@sineswingers.com>
2018-05-26 20:04:21 +0200

    Alias HashMap and replace String::from() with .to_string()

--- src/lib.rs
+++ src/lib.rs
@@ -106,70 +106,67 @@ impl<'a> Config<'a> {
     }
 }
 
-fn location_map() -> Result<HashMap<String, String>, util::Error> {
-    let mut location_map: HashMap<String, String> = HashMap::new();
-    location_map.insert(String::from("MC100"), String::from("Tammela, canopy"));
-    location_map.insert(String::from("MC101"), String::from("Tammela, ground"));
-    location_map.insert(String::from("MC102"), String::from("Tammela, crown"));
+type LocationMap = HashMap<String, String>;
+fn location_map() -> Result<LocationMap, util::Error> {
+    let mut location_map: LocationMap = HashMap::new();
+    location_map.insert("MC100".to_string(), "Tammela, canopy".to_string());
+    location_map.insert("MC101".to_string(), "Tammela, ground".to_string());
+    location_map.insert("MC102".to_string(), "Tammela, crown".to_string());
 
-    location_map.insert(String::from("MC103"), String::from("Punkaharju, ground"));
-    location_map.insert(String::from("MC104"), String::from("Punkaharju, crown"));
-    location_map.insert(String::from("MC105"), String::from("Punkaharju, landscape"));
+    location_map.insert("MC103".to_string(), "Punkaharju, ground".to_string());
+    location_map.insert("MC104".to_string(), "Punkaharju, crown".to_string());
+    location_map.insert("MC105".to_string(), "Punkaharju, landscape".to_string());
 
-    location_map.insert(String::from("MC106"), String::from("Hyytiälä, crown"));
-    location_map.insert(String::from("MC107"), String::from("Hyytiälä, ground"));
+    location_map.insert("MC106".to_string(), "Hyytiälä, crown".to_string());
+    location_map.insert("MC107".to_string(), "Hyytiälä, ground".to_string());
 
-    location_map.insert(
-        String::from("MC108"),
-        String::from("Sodankylä, forest, canopy"),
-    );
-    location_map.insert(
-        String::from("MC109"),
-        String::from("Sodankylä, forest, crown"),
-    );
-    location_map.insert(
-        String::from("MC110"),
-        String::from("Sodankylä, forest, ground"),
-    );
+    location_map.insert("MC100".to_string(), "Tammela, canopy".to_string());
+    location_map.insert("MC101".to_string(), "Tammela, ground".to_string());
 
     location_map.insert(
-        String::from("MC111"),
-        String::from("Sodankylä, wetland, ground"),
+        "MC108".to_string(),
+        "Sodankylä, forest, canopy".to_string(),
     );
-
-    location_map.insert(String::from("MC112"), String::from("Parkano, landscape"));
-
-    location_map.insert(String::from("MC113"), String::from("Suonenjoki, canopy"));
-
-    location_map.insert(String::from("MC114"), String::from("Kenttärova, canopy"));
-    location_map.insert(String::from("MC115"), String::from("Kenttärova, crown"));
-    location_map.insert(String::from("MC116"), String::from("Kenttärova, ground"));
-
-    location_map.insert(String::from("MC117"), String::from("Paljakka, landscape"));
-    location_map.insert(String::from("MC118"), String::from("Paljakka, landscape"));
-    location_map.insert(String::from("MC117-1"), String::from("Paljakka, landscape"));
-
-    location_map.insert(String::from("MC119"), String::from("Värriö, canopy"));
-    location_map.insert(String::from("MC120"), String::from("Värriö, crown"));
-    location_map.insert(String::from("MC121"), String::from("Värriö, ground"));
-
-    location_map.insert(String::from("MC122"), String::from("Lammi, crown"));
-    location_map.insert(String::from("MC123"), String::from("Lammi, crown"));
-    location_map.insert(String::from("MC124"), String::from("Lammi, landscape"));
-    location_map.insert(String::from("MC125"), String::from("Lammi, landscape"));
-    location_map.insert(String::from("MC126"), String::from("Lammi, ground"));
-    location_map.insert(String::from("MC127"), String::from("Lammi, ground"));
-
-    location_map.insert(String::from("MC128"), String::from("Kaamanen, ground"));
-
+    location_map.insert("MC109".to_string(), "Sodankylä, forest, crown".to_string());
     location_map.insert(
-        String::from("MC129"),
-        String::from("Lompolojänkkä, ground"),
+        "MC110".to_string(),
+        "Sodankylä, forest, ground".to_string(),
+    );
+    location_map.insert(
+        "MC111".to_string(),
+        "Sodankylä, wetland, ground".to_string(),
     );
 
-    location_map.insert(String::from("MC130"), String::from("Tvärminne, landscape"));
+    location_map.insert("MC112".to_string(), "Parkano, landscape".to_string());
 
-    location_map.insert(String::from("MC131"), String::from("Jokioinen, landscape"));
+    location_map.insert("MC113".to_string(), "Suonenjoki, canopy".to_string());
+
+    location_map.insert("MC114".to_string(), "Kenttärova, canopy".to_string());
+    location_map.insert("MC115".to_string(), "Kenttärova, crown".to_string());
+    location_map.insert("MC116".to_string(), "Kenttärova, ground".to_string());
+
+    location_map.insert("MC117".to_string(), "Paljakka, landscape".to_string());
+    location_map.insert("MC118".to_string(), "Paljakka, landscape".to_string());
+    location_map.insert("MC117-1".to_string(), "Paljakka, landscape".to_string());
+
+    location_map.insert("MC119".to_string(), "Värriö, canopy".to_string());
+    location_map.insert("MC120".to_string(), "Värriö, crown".to_string());
+    location_map.insert("MC121".to_string(), "Värriö, ground".to_string());
+
+    location_map.insert("MC122".to_string(), "Lammi, crown".to_string());
+    location_map.insert("MC123".to_string(), "Lammi, crown".to_string());
+    location_map.insert("MC124".to_string(), "Lammi, landscape".to_string());
+    location_map.insert("MC125".to_string(), "Lammi, landscape".to_string());
+    location_map.insert("MC126".to_string(), "Lammi, ground".to_string());
+    location_map.insert("MC127".to_string(), "Lammi, ground".to_string());
+
+    location_map.insert("MC128".to_string(), "Kaamanen, ground".to_string());
+
+    location_map.insert("MC129".to_string(), "Lompolojänkkä, ground".to_string());
+
+    location_map.insert("MC130".to_string(), "Tvärminne, landscape".to_string());
+
+    location_map.insert("MC131".to_string(), "Jokioinen, landscape".to_string());
 
     Ok(location_map)
 }
